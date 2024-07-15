7e778e5f3445000d0bf8f1be6f3de7c92fc2f964
jessdebrito <debrito.herself@gmail.com>
2024-07-15 03:40:58 -0300

    monorepo

---
+++ tiktok-feed/README.md
@@ -0,0 +1,4 @@
+# React + TypeScript + Vite + Tailwind
+
+Repository used to study and test the library.
+It probably won't make sense.
---
+++ tiktok-feed/src/components/FeedGrid/index.tsx
@@ -0,0 +1,23 @@
+import FeedCard from "../feedCard";
+function FeedGrid() {
+    return ( 
+        <div className="grid grid-cols-3 gap-4">
+
+            <FeedCard title="Foto Doguinho"/>
+            <FeedCard title="Foto Doguinho"/>
+            <FeedCard title="Foto Doguinho"/>
+            
+            <FeedCard title="Foto Doguinho"/>
+            <FeedCard title="Foto Doguinho"/>
+            <FeedCard title="Foto Doguinho"/>
+            
+            <FeedCard title="Foto Doguinho"/>
+            <FeedCard title="Foto Doguinho"/>
+            <FeedCard title="Foto Doguinho"/>
+            
+
+        </div>
+     );
+}
+
+export default FeedGrid;
